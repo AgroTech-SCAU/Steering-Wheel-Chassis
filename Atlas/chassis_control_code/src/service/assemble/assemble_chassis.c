316ed39fdee2897fc5d022bcedeb365354168a29
KaguraKaerei <kaerei86@gmail.com>
2026-06-12 23:58:25 +0800

    feat(k_vy): 调节 k_vy 参数

--- Atlas/chassis_control_code/src/service/assemble/assemble_chassis.c
+++ Atlas/chassis_control_code/src/service/assemble/assemble_chassis.c
@@ -49,8 +49,9 @@ SystemStatus assemble_chassis(void) {
         .steer_target_mode = CHASSIS_STEER_TARGET_ABS_NEAREST,
         .yaw_bias = {
             .enabled = true,
-            .k_vx = -0.049f,
-            .k_vy = -0.028f,
+            // .k_vx = -0.049f,
+            .k_vx = 0.0f,
+            .k_vy = 0.042f,
             .v_deadband = 0.01f,
         }
     };
